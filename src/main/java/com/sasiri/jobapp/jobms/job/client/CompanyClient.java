0c9f8596a14af8af199dcaed04e3f8ee8b27ab33
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-07-05 10:24:55 +0530

    add Kubernetes env properties as application-k8s.properties

--- src/main/java/com/sasiri/jobapp/jobms/job/client/CompanyClient.java
+++ src/main/java/com/sasiri/jobapp/jobms/job/client/CompanyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "companyms")
+@FeignClient(name = "companyms",url = "${company-service.url}")
 public interface CompanyClient {
     @GetMapping("/companies/{id}")
      Company getCompany(@PathVariable Long id);
--- src/main/java/com/sasiri/jobapp/jobms/job/client/ReviewClient.java
+++ src/main/java/com/sasiri/jobapp/jobms/job/client/ReviewClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "reviewms")
+@FeignClient(name = "reviewms",url = "${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews")
     List<Review> getReviews(@RequestParam("companyId") Long companyId);
